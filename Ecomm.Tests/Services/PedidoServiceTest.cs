9fbdfc229d6abcfd5865d03810e821ec508b8a47
luizolivas <luizolivas@gmail.com>
2024-07-20 17:10:12 -0300

    Create test class Pedido

---
+++ Ecomm.Tests/Services/PedidoServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using EcommProject.Context;
+using EcommProject.Dtos;
+using EcommProject.Models;
+using EcommProject.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecomm.Tests.Services
+{
+    public class PedidoServiceTest
+    {
+        private PedidoService pedidoService;
+        private ApplicationDbContext context;
+        private IMapper mapper;
+        private Mock<IRabbitService> mockRabbitService;
+
+        public PedidoServiceTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            context = new ApplicationDbContext(options);
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<PedidoDto, Pedido>();
+                cfg.CreateMap<Pedido, PedidoResponseDto>();
+            });
+
+
+            mockRabbitService = new Mock<IRabbitService>();
+
+            pedidoService = new PedidoService(context, mapper, mockRabbitService.Object);
+        }
+    }
+}

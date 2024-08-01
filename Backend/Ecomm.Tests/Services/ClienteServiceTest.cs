f57b56d078d9cfb959d120702a858d707404cacf
luizolivas <luizolivas@gmail.com>
2024-08-01 18:08:05 -0300

    Add Cliente tests

--- Backend/Ecomm.Tests/Services/ClienteServiceTest.cs
+++ Backend/Ecomm.Tests/Services/ClienteServiceTest.cs
@@ -49,8 +49,69 @@ namespace Ecomm.Tests.Services
 
             Assert.NotNull(clienteResult);
             Assert.Equal(cliente.Name, clienteResult.Name);
+
+            _dbContext.Database.EnsureDeleted();
             
         }
-        
+
+
+        [Fact]
+        public async Task Get_GetAllPedidos()
+        {
+            List<ClienteDTO> clientesDto = new List<ClienteDTO>
+            {
+                new ClienteDTO { Name = "Test1" },
+                new ClienteDTO { Name = "Test2" }
+            };
+
+
+
+            foreach (var cliente in clientesDto)
+            {
+                await _clienteService.AddCliente(cliente);
+            }
+            await _dbContext.SaveChangesAsync();
+
+            var result = await _clienteService.GetAllClientes();
+
+            Assert.Equal(2, result.Count);
+            Assert.NotNull(result);
+            Assert.Equal(clientesDto.Count, result.Count);
+
+            for (int i = 0; i < clientesDto.Count; i++)
+            {
+                Assert.Equal(clientesDto[i].Name, result[i].Name);
+            }
+
+            _dbContext.Database.EnsureDeleted();
+        }
+
+        [Fact]
+        public async Task Get_GetClienteById()
+        {
+            List<ClienteDTO> clientesDto = new List<ClienteDTO>
+            {
+                new ClienteDTO { Name = "Test1" },
+                new ClienteDTO { Name = "Test2" },
+                new ClienteDTO { Name = "Test3" },
+                new ClienteDTO { Name = "Test4" }
+            };
+
+            foreach (var cliente in clientesDto)
+            {
+                await _clienteService.AddCliente(cliente);
+            }
+            await _dbContext.SaveChangesAsync();
+
+
+            var result = await _clienteService.GetCliente(3);
+
+            Assert.NotNull(result);
+            Assert.Equal("Test3", result.Name);
+
+            _dbContext.Database.EnsureDeleted();
+
+        }
+
     }
 }
